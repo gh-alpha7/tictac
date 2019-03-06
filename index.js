90c27dd36512176ed1fed6953dc47bf8fb65fdbf
subh1685 <shubhamsngh067@gmail.com>
2019-03-07 02:21:40 +0530

    changes

--- index.js
+++ index.js
@@ -14,14 +14,17 @@
 * Winner needs to be decided and has to be flashed
 *
 * Extra points will be given for approaching the problem more creatively
-* 
+*
 */
 
-const grid = [];
+let grid = [];
 const GRID_LENGTH = 3;
-let turn = 'X';
+let turn = 'X',turnCount=0;
+
+
 
 function initializeGrid() {
+
     for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
         const tempArray = [];
         for (let rowidx = 0; rowidx < GRID_LENGTH;rowidx++) {
@@ -32,8 +35,9 @@ function initializeGrid() {
 }
 
 function getRowBoxes(colIdx) {
+
     let rowDivs = '';
-    
+
     for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
         let additionalClass = 'darkBackground';
         let content = '';
@@ -55,6 +59,7 @@ function getRowBoxes(colIdx) {
 }
 
 function getColumns() {
+
     let columnDivs = '';
     for(let colIdx=0; colIdx < GRID_LENGTH; colIdx++) {
         let coldiv = getRowBoxes(colIdx);
@@ -65,21 +70,98 @@ function getColumns() {
 }
 
 function renderMainGrid() {
+
     const parent = document.getElementById("grid");
     const columnDivs = getColumns();
     parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
 }
 
 function onBoxClick() {
+
     var rowIdx = this.getAttribute("rowIdx");
     var colIdx = this.getAttribute("colIdx");
-    let newValue = 1;
+
+    let newValue =1
+    turnCount+=1
     grid[colIdx][rowIdx] = newValue;
     renderMainGrid();
     addClickHandlers();
+    if(!winCheck()){
+      if(turnCount<9)
+        computerTurn();
+      else{
+        document.getElementById("winner").innerHTML="New Game"
+        resetGame()
+    }
+  }
+
+}
+
+function computerTurn(){
+  let x=0,y=0
+  while(grid[x][y])
+    x=Math.floor(Math.random() * 3 ),y=Math.floor(Math.random() * 3 )
+  turnCount+=1
+  grid[x][y]=2
+  renderMainGrid();
+  addClickHandlers();
+  winCheck();
+}
+
+function winCheck(){
+  if(computerWin()){
+    document.getElementById("winner").innerHTML="COMPUTER WON"
+    resetGame()
+    return true
+  }
+  else if(humanWin()){
+    document.getElementById("winner").innerHTML="HUMAN WON"
+    resetGame()
+    return true
+  }
+  return false
+}
+
+function computerWin(){
+  for(var i=0;i<3;i++){
+    if((grid[i][0]==grid[i][1]&&grid[i][1]==grid[i][2]&&grid[i][2]==2)||(grid[0][i]==grid[1][i]&&grid[1][i]==grid[2][i]&&grid[2][i]==2)){
+      return true;
+    }
+
+  }
+  if(grid[0][0]==grid[1][1]&&grid[1][1]==grid[2][2]&&grid[2][2]==2){
+    return true;
+  }
+  if(grid[0][2]==grid[1][1]&&grid[1][1]==grid[2][0]&&grid[2][0]==2){
+    return true;
+  }
+}
+
+function humanWin(){
+  for(var i=0;i<3;i++){
+    if((grid[i][0]==grid[i][1]&&grid[i][1]==grid[i][2]&&grid[i][2]==1)||(grid[0][i]==grid[1][i]&&grid[1][i]==grid[2][i]&&grid[2][i]==1)){
+      return true;
+    }
+  }
+  if(grid[0][0]==grid[1][1]&&grid[1][1]==grid[2][2]&&grid[2][2]==1){
+    return true;
+  }
+  if(grid[0][2]==grid[1][1]&&grid[1][1]==grid[2][0]&&grid[2][0]==1){
+    return true;
+  }
+}
+
+function resetGame(){
+  grid=[]
+  initializeGrid();
+  renderMainGrid();
+  addClickHandlers();
+  turnCount=0
+
 }
 
 function addClickHandlers() {
+
     var boxes = document.getElementsByClassName("box");
     for (var idx = 0; idx < boxes.length; idx++) {
         boxes[idx].addEventListener('click', onBoxClick, false);
